8bdf944cdfb139e6a177aa09d03b5a799dcab0cc
Lee-hwansoo <lee_hwansoo@naver.com>
2024-03-28 14:27:54 +0900

    fix : launch file

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ using PREC = float;
 
 int32_t main(int32_t argc, char** argv)
 {
-    ros::init(argc, argv, "lane_keeping_system");
+    ros::init(argc, argv, "Lane_Keeping_System");
     Xycar::LaneKeepingSystem<PREC> laneKeepingSystem;
     laneKeepingSystem.run();
 
